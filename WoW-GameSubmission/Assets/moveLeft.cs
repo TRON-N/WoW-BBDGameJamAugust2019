53548013bc98669fe81fb3bb21c3efb54965f62d
bbdnet1958 <bbdnet1958@LEONARDVGE>
2019-08-11 21:01:44 +0200

    I replaced the strange CD scence with my proof of concept bouncy ball

---
+++ WoW-GameSubmission/Assets/moveLeft.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveLeft : MonoBehaviour
+{
+    public Vector3 initVel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<Rigidbody2D>().velocity = initVel;
+    }
+
+    void Update()
+    {
+        Vector3 currentVel = this.GetComponent<Rigidbody2D>().velocity;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            currentVel.x -= 0.05f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            currentVel.x += 0.05f;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (currentVel.y < 0)
+            {
+                currentVel.y -= 0.5f;
+            }
+        }
+
+        this.GetComponent<Rigidbody2D>().velocity = currentVel;
+    }
+}
