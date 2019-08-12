1eafa0d4b46d03b538ee66659283f3e21f400875
bbdnet1882 <Darius@bbd.co.za>
2019-08-12 16:48:23 +0200

    Added Darius demo

---
+++ Demo1/Assets/BigBallController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigBallController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        addForce();
+    }
+
+    void addForce()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(new Vector2(-5f, 0));
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(new Vector2(5f, 0));
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(new Vector2(0, -15f));
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(new Vector2(0, 5f));
+        }
+    }
+    
+}
---
+++ Demo1/Assets/SmallBallController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallBallController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 rbVelocity = rb.position;
+        // setVelocity(rbVelocity);
+        addForce();
+        // addRelativeForce(rbVelocity);
+    }
+
+    void setVelocity(Vector2 rbVelocity)
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            rb.velocity = new Vector2 (-5f, rbVelocity.y);
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            rb.velocity = new Vector2 (5f, rbVelocity.y);
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
+            rb.velocity = new Vector2 (rbVelocity.x, -5f);
+        } else if (Input.GetKey(KeyCode.UpArrow)) {
+            rb.velocity = new Vector2 (rbVelocity.x, 5f);
+        }
+    }
+
+    void addForce()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            rb.AddForce(new Vector2(-5f, 0));
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            rb.AddForce(new Vector2(5f, 0));
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
+            rb.AddForce(new Vector2(0, -15f));
+        } else if (Input.GetKey(KeyCode.UpArrow)) {
+            rb.AddForce(new Vector2(0, 5f));
+        }
+    }
+
+    void addRelativeForce(Vector2 rbVelocity)
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            rb.AddRelativeForce(new Vector2(-5f, rbVelocity.y));
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            rb.AddRelativeForce(new Vector2(5f, rbVelocity.y));
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
+            rb.AddRelativeForce(new Vector2(rbVelocity.x, -5f));
+        } else if (Input.GetKey(KeyCode.UpArrow)) {
+            rb.AddRelativeForce(new Vector2(rbVelocity.x, 5f));
+        }
+    }
+}
