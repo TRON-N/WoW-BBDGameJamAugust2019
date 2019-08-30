caff6c150d46b8ad1a69c8853eaa601d295edffd
bbdnet1958 <bbdnet1958@LEONARDVGE>
2019-08-30 23:34:11 +0200

    fixed the sizeing?

--- WoW-GameSubmission/Assets/timerScript.cs
+++ WoW-GameSubmission/Assets/timerScript.cs
@@ -5,12 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class timerScript : MonoBehaviour
 {
-
     public float timeLeft = 10.0f;
     public Text startText; // used for showing countdown from 3, 2, 1
-    public UIManager uiManager; 
+    public UIManager uiManager;
+    public SpriteRenderer side;
 
+    void Start()
+    {
+        float orthoSize = this.side.bounds.size.x * Screen.height / Screen.width * 0.5f;
 
+        Camera.main.orthographicSize = orthoSize;
+    }
     void Update()
     {
         timeLeft -= Time.deltaTime;
